ac99e347522932d34cf73b49345fc3065bafcf95
LMERLAUD <louis.merlaud@ynov.com>
2022-08-30 19:42:11 +0200

    append in json work, display json info work in progress

--- add.go
+++ add.go
@@ -1,42 +1,72 @@
 package main
 
 import (
-	"os"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
+type Game struct {
+	GameName string `json:"GameName"`
+	Path string `json:"Path"`
+}
+
 func Display_add_game_window(w fyne.Window){
+
+	game_name := widget.NewEntry()
+	game_name.SetPlaceHolder("Enter game name")
 
 	input := widget.NewEntry()
 	input.SetPlaceHolder("Enter game binary path")
 
 	w.SetContent(container.NewVBox(
-		input, 
+		input,
+		game_name,
 		widget.NewButton("add a game", func() {
-		write_game_path(input.Text)
+		write_game_path(game_name.Text, input.Text)
 		input.Text = ""
+		game_name.Text = ""
 	}),))
 	w.Resize(fyne.NewSize(800,400))
 	w.Show()
 }
 
-func write_game_path(txt string){
-	path := []byte(txt + "\n")
-	// err := ioutil.WriteFile("game_path.txt", path, 0644)
+func write_game_path(name string, txt string){
+	// path := []byte(txt + "\n")
+	// f, err := os.OpenFile("game_path.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	// if err != nil {
-    //     fmt.Println(err)
-    // }
-	f, err := os.OpenFile("game_path.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+	// 	panic(err)
+	// }
+
+	// defer f.Close()
+
+	// if _, err = f.WriteString(string(path)); err != nil {
+	// 	panic(err)
+	// }
+
+	file, _ := ioutil.ReadFile("data.json")
+
+	data := []Game{}
+
+    json.Unmarshal(file, &data)
+
+    newStruct := &Game{
+		GameName: name,
+		Path: txt,
+    }
+
+	data = append(data, *newStruct)
+
+	file, err := json.Marshal(data)
 	if err != nil {
-		panic(err)
-	}
-
-	defer f.Close()
-
-	if _, err = f.WriteString(string(path)); err != nil {
-		panic(err)
-	}
+        fmt.Println(err)
+    }
+	err = ioutil.WriteFile("data.json", file, 0644)
+	if err != nil {
+        fmt.Println(err)
+    }
 }
--- main.go
+++ main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
-	"bufio"
+	"encoding/json"
 	"fmt"
-	"log"
-	"os"
+	"image/color"
+	"io/ioutil"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
 )
 
+type myTheme struct {}
+
+type Data struct {
+	Data []Game `json:"datas"`
+}
+
 func Display_laucher(){
+	// var _ fyne.Theme = (*myTheme)(nil)
 	a := app.New()
 	w := a.NewWindow("welcome to cajoux laucher")
 	var s *fyne.Container
@@ -28,6 +36,7 @@ func Display_laucher(){
 
 	Display_game(w,s)
 	w.Resize(fyne.NewSize(800,400))
+	// a.Settings().SetTheme(&myTheme{})
 	w.ShowAndRun()
 }
 
@@ -35,22 +44,26 @@ func Display_laucher(){
 func Display_game(w fyne.Window, s *fyne.Container){
 
 		//read game_path.txt
-		f, err := os.Open("game_path.txt")
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer f.Close()
-		scanner := bufio.NewScanner(f)
+		f, _ := ioutil.ReadFile("data.json")
+		datas := Data{}
+		_ = json.Unmarshal([]byte(f), &datas)
+		//scanner := bufio.NewScanner(f)
 
 		var s2 *fyne.Container
 		var box *container.Scroll
 		s2 = container.New(layout.NewVBoxLayout())
 
 		//display games layout
-		for scanner.Scan() {
-			fmt.Println(scanner.Text())
-			x := widget.NewButton(scanner.Text(), func() {})
-			s2.Add(x)
+		for i := 0; i < len(datas.Data); i++ {
+			fmt.Println(datas.Data[i])
+			var s3 *fyne.Container
+			s3 = container.New(layout.NewGridLayout(3))
+			label := widget.NewLabel(datas.Data[i].GameName)
+			x := widget.NewButton(datas.Data[i].Path, func() {})
+			s3.Add(label)
+			s3.Add(x)
+
+			s2.Add(s3)
 		}
 
 		box = container.NewVScroll(s2)
@@ -61,9 +74,20 @@ func Display_game(w fyne.Window, s *fyne.Container){
 			box,
 		),)
 
-		if err := scanner.Err(); err != nil {
-			log.Fatal(err)
+		// if err := scanner.Err(); err != nil {
+		// 	log.Fatal(err)
+		// }
+}
+
+func (m myTheme) Color(name fyne.ThemeColorName, variant fyne.ThemeVariant) color.Color {
+	if name == theme.ColorNameBackground {
+		if variant == theme.VariantLight {
+			return color.White
 		}
+		return color.Black
+	}
+
+	return theme.DefaultTheme().Color(name, variant)
 }
 
 func search(){
